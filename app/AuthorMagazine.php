1aa053cd3fe112f596c1705c25d28805828388cd
beluana@mail.ru <vehksrf5577>
2020-12-24 23:04:39 +0500

    pivot model change

--- app/AuthorMagazine.php
+++ app/AuthorMagazine.php
@@ -1,10 +1,13 @@
 <?php
 
-
 namespace App;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-
-class AuthorMagazine
+class AuthorMagazine extends Pivot
 {
+    protected $table = 'author_magazine';
 
+    protected $fillable = [
+        'author_id', 'magazine_id'
+    ];
 }
